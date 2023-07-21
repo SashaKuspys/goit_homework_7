7a3b2c44508ce41a2b1f2850b570506d003c3b35
SashaKuspys <134163412+SashaKuspys@users.noreply.github.com>
2023-07-21 17:05:59 +0300

    HomeWork_7

---
+++ clean_folder_kobSK/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="clean_folder_kobSK",
+    version="0.0.1",
+    description="organizes folders and files",
+    author="Oleksandr Kuspys",
+    author_email="sashakuspys@gmail.com",
+    packages=find_namespace_packages(),
+    entry_points={"console_scripts": ["clean_folder=clean_folder_kobSK.main:start"]},
+)
